0ee10a4858047c2b6e3a857df79349b38e601f3e
Hayden Lauritzen <haydenlauritzen@gmail.com>
2022-12-20 17:42:31 -0800

    init project

---
+++ src/board.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file board.cpp
+ * @author your name (you@domain.com)
+ * @brief
+ * @version 0.1
+ * @date 2022-12-20
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+class Board {
+
+private:
+    char board[]; // 
+
+public:
+
+};
---
+++ src/engine.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file engine.cpp
+ * @author Hayden Lauritzen (haydenlauritzen@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-12-20
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <iostream>
+
+int main () {
+    
+}
